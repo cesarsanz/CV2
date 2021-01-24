f65487404a5fa7bc8cb6d968e7a9cbfa6f4f00e5
cesarsanz <77105135+cesarsanz@users.noreply.github.com>
2021-01-24 16:33:56 +0100

    Ejercicio1.2
    Ejercicio 1.2 tema 2 Introducción al JS

---
+++ Ejercicio1.2/myscript.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const HB = {
+  day: 25,
+  month: 'Octubre'
+};
+console.log('Mi cumpleaños es el ' + HB.day + ' de '+ HB.month);
+
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+console.log('El mes siguietne a mi cumpleajos es' + meses[10]);
