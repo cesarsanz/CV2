5345da3993f66b4f3f00d845cfdbe91a62c7b36e
cesarsanz <77105135+cesarsanz@users.noreply.github.com>
2021-01-25 08:16:01 +0100

    Ejercicios Tema 2 1.3
    Ejercicio 1.3 del tema 2

---
+++ Ejercicio1.3/myscript.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const data = ['hola', 2, 5, 'adios'];
+
+// Determina cuál de las dos cadenas que hay en el array es mayor.
+console.log('5 es mayor que 2', data[2]>data[1]);
+
+// Operaciones numéricas
+console.log('Suma 2 + 5=', data[1]+data[2]);
+console.log('Resta 2 - 5=', data[1]-data[2]);
+console.log('Multiplicación 2*5=', data[1]*data[2]);
+console.log('División 2/5=', data[1]/data[2]);
+console.log('Resto de 5/2=', data[2]%data[1]);
